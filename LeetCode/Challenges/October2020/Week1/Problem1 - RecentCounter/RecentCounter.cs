cec80e619c01d5de0843d76d7931e652c67666c9
gavrihaddad <gavrihaddad@gmail.com>
2020-10-11 17:56:28 +0300

    October 2020 Week 1 Challenge 1: RecentCounter.

---
+++ LeetCode/Challenges/October2020/Week1/Problem1 - RecentCounter/RecentCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Chalnges.October2020.Week1.Problem1___RecentCounter
+{
+    public static class RecentCounter
+    {
+        public static List<int> RecentRequestTimes;
+        public static int currentIndex;
+
+        static RecentCounter()
+        {
+            currentIndex = 0;
+            RecentRequestTimes = new List<int>();
+        }
+
+        public static int Ping(int t)
+        {
+            RecentRequestTimes.Add(t);
+
+            while (currentIndex < RecentRequestTimes.Count &&
+                   t - RecentRequestTimes[currentIndex] > 3000)
+            {
+                currentIndex++;
+            }
+
+            return RecentRequestTimes.Count - currentIndex;
+        }
+    }
+}
