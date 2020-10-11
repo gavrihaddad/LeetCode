c5fd3935e04de30caec29e7ed8892cd6c898e666
gavrihaddad <gavrihaddad@gmail.com>
2020-10-11 17:40:13 +0300

    Problem 1: Tow Sum

---
+++ LeetCode/Problems/Algorithms/Problem1 - TowSum/TowSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms.Problem1___TowSum
+{
+    /// <summary>
+    /// Solves problem 1.
+    /// Accepted by LeetCode.
+    /// </summary>
+    public static class TowSum
+    {
+        public static int[] GetTwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Dictionary<int, int> mapping = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int match = target - nums[i];
+
+                if (mapping.ContainsKey(match))
+                {
+                    result[0] = i;
+                    result[1] = mapping[match];
+
+                    break;
+                }
+
+                if (mapping.ContainsKey(nums[i]) == false)
+                {
+                    mapping.Add(nums[i], i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
