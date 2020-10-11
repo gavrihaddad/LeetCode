571ee8c47710497cc29e3b179ec1eb4578403e29
gavrihaddad <gavrihaddad@gmail.com>
2020-10-11 17:57:26 +0300

    October 2020 Week 2 Challenge 1: BinarySearch.

---
+++ LeetCode/Challenges/October2020/Week2/Problem1 - BinarySearch/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Chalnges.October2020.Week2.Problem1___BinarySearch
+{
+    /// <summary>
+    /// Solves "BinarySearch".
+    /// October 8 2020.
+    /// Accepted by LeetCode.
+    /// </summary>
+    public static class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            int result = -1;
+            int leftLimit = 0;
+            int rightLimit = nums.Length - 1;
+
+            while (leftLimit <= rightLimit)
+            {
+                int middleIndex = (leftLimit + rightLimit) / 2;
+
+                if(nums[middleIndex] == target)
+                {
+                    return middleIndex;
+                }
+                else if (nums[middleIndex] < target)
+                {
+                    leftLimit = middleIndex + 1;
+                }
+                else if (nums[middleIndex] > target)
+                {
+                    rightLimit = middleIndex - 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
