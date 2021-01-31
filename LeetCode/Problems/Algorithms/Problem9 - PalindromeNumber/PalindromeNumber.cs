910834e8612bdc135d15a819204d89d6be54dabc
gavriel haddadGh3220010130 <gavrihaddad@gmail.com>
2021-01-31 18:46:08 +0200

    Problem 9: Palindromic Number

---
+++ LeetCode/Problems/Algorithms/Problem9 - PalindromeNumber/PalindromeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms.Problem9___PalindromeNumber
+{
+    /// <summary>
+    /// Solves problem 9.
+    /// Accepted by LeetCode.
+    /// Runtime: 52 ms, faster than 97.47% of C# online submissions for Palindrome Number.(LeetCode submission message).
+    /// </summary>
+    public static class PalindromeNumber
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x >= 0)
+            {
+                int origin = x;
+                int rvrsNum = 0;
+
+                while (x > 0)
+                {
+                    rvrsNum *= 10;
+                    rvrsNum += x % 10;
+                    x /= 10;
+                }
+
+                if (origin == rvrsNum)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,4 @@
-﻿using LeetCode.Challenges.October2020.Week3.Problem2___Search_Matrix;
-using LeetCode.Problems.Algorithms.Problem2___AddTwoNumbers;
-using LeetCode.Problems.Algorithms.Problem3___LongestSubstringWithoutRepeatingCharacters;
-using LeetCode.Problems.Algorithms.Problem4___MedianOfArrays;
-using LeetCode.Problems.Algorithms.Problem5___LongestPalindromicSubstring;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace LeetCode
 {
@@ -12,7 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(LongestPalindromicSubstring.LongestPalindrome("whdqcudjpisufnrtsyupwtnnbsvfptrcgvobbjglmpynebblpigaflpbezjvjgbmofejyjssdhbgghgrhzuplbeptpaecfdanhlylgusptlgobkqnulxvnwuzwauewcplnvcwowmbxxnhsdmgxtvbfgnuqdpxennqglgmspbagvmjcmzmbsuacxlqfxjggrwsnbblnnwisvmpwwhomyjylbtedzrptejjsaiqzprnadkjxeqfdpkddmbzokkegtypxaafodjdwirynzurzkjzrkufsokhcdkajwmqvhcbzcnysrbsfxhfvtodqabvbuosxtonbpmgoemcgkudandrioncjigbyizekiakmrfjvezuzddjxqyevyenuebfwugqelxwpirsoyixowcmtgosuggrkdciehktojageynqkazsqxraimeopcsjxcdtzhlbvtlvzytgblwkmbfwmggrkpioeofkrmfdgfwknrbaimhefpzckrzwdvddhdqujffwvtvfyjlimkljrsnnhudyejcrtrwvtsbkxaplchgbikscfcbhovlepdojmqybzhbiionyjxqsmquehkhzdiawfxunguhqhkxqdiiwsbuhosebxrpcstpklukjcsnnzpbylzaoyrmyjatuovmaqiwfdfwyhugbeehdzeozdrvcvghekusiahfxhlzclhbegdnvkzeoafodnqbtanfwixjzirnoaiqamjgkcapeopbzbgtxsjhqurbpbuduqjziznblrhxbydxsmtjdfeepntijqpkuwmqezkhnkwbvwgnkxmkyhlbfuwaslmjzlhocsgtoujabbexvxweigplmlewumcone"));
+            Console.WriteLine();
 
             Console.ReadKey();
         }
