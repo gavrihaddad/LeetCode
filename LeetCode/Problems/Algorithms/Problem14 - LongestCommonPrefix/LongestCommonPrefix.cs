4c701354f5c0fb28b7b99fbe5fad40642ee43e19
ghaddad193 <ghaddad193@gmail.com>
2021-03-18 16:25:13 +0200

    Problem 14: Longest Common Prefix.

---
+++ LeetCode/Problems/Algorithms/Problem14 - LongestCommonPrefix/LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms.Problem14___LongestCommonPrefix
+{
+    class LongestCommonPrefix
+    {
+        static public string GetLongestCommonPrefix(string[] strs)
+        {
+            string prefix = "";
+
+            if (strs.Length > 0)
+            {
+                for (int i = 0; i < strs[0].Length; i++)
+                {
+                    char letter = strs[0][i];
+                    for (int j = 1; j < strs.Length; j++)
+                    {
+                        if (strs[j].Length == i || strs[j][i] != letter)
+                        {
+                            return prefix;
+                        }
+                    }
+
+                    prefix += letter;
+                }
+            }
+
+            return prefix;
+        }
+
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 
 namespace LeetCode
 {
+    //Runtime: 88 ms, faster than 99.47% of C# online submissions for Longest Common Prefix.
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RomanToInt());
+            Console.WriteLine();
 
             Console.ReadKey();
         }
