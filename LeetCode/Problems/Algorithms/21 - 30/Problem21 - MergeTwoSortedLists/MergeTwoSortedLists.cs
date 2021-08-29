f2d8209c4208e89d7fc46bca77a7f92a20cdf2f3
gavrihaddad <gavrihaddad@gmail.com>
2021-08-29 15:11:21 +0300

    Problem 21: Merge Two Sorted Lists.

---
+++ LeetCode/Problems/Algorithms/21 - 30/Problem21 - MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms._21___30.Problem21___MergeTwoSortedLists
+{
+    /// <summary>
+    /// Solves problem 21.
+    /// Accepted by LeetCode.
+    /// Runtime: 84 ms, faster than 97.65% of C# online submissions (LeetCode submission message).
+    /// </summary>
+    public static class MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+            {
+                return l2;
+            }
+            else if (l2 == null)
+            {
+                return l1;
+            }
+            else if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,7 +9,6 @@ namespace LeetCode
         static void Main(string[] args)
         {
 
-
             Console.ReadKey();
         }
     }
