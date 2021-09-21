090c527223924922d3279054f3e2dd0692820c38
gavrihaddad <gavrihaddad@gmail.com>
2021-09-21 20:05:05 +0300

    Problem 27: Remove Element.

---
+++ LeetCode/Problems/Algorithms/21 - 30/Problem27 - RemoveElement/RemoveElement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms._21___30.Problem27___RemoveElement
+{
+    /// <summary>
+    /// Solves problem 26.
+    /// Accepted by LeetCode.
+    /// Runtime: 228 ms, faster than 94.92% of C# online submissions (LeetCode submission message).
+    /// </summary>
+    public static class RemoveElement
+    {
+        public static int RemoveElement1(int[] nums, int val)
+        {
+            if (Array.FindIndex(nums, item => item == val) == -1)
+            {
+                return nums.Length;
+            }
+            else
+            {
+                int numIndex = 0;
+                while (numIndex != -1)
+                {
+                    int valIndex = Array.FindIndex(nums, item => item == val);
+                    numIndex = Array.FindIndex(nums, valIndex, item => item != val);
+
+                    if (numIndex != -1)
+                    {
+                        nums[valIndex] = nums[numIndex];
+                        nums[numIndex] = val;
+                    }
+                }
+
+                return Array.FindIndex(nums, item => item == val);
+            }
+        }
+    }
+}
