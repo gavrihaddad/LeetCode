3314557184f3f15f0e0c02d785b67e15bbe6f566
gavrihaddad <gavrihaddad@gmail.com>
2021-09-30 21:39:02 +0300

    Problem 50: Pow(x,n).

---
+++ LeetCode/Problems/Algorithms/1 - 1000/1 - 100/41 - 50/Problem50 - Pow(x, n)/PowXN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Algorithms._1___1000._1___100._41___50.Problem50___Pow_x__n_
+{
+    //TODO: ברצינות?
+    class PowXN
+    {
+        //Runtime: 40 ms, faster than 96.83% of C# online submissions for Pow(x, n).
+        public double MyPow(double x, int n)
+        {
+            return Math.Pow(x, n);
+        }
+    }
+}
